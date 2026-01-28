39c34025920ce69101c94d4ccd10fbc0a15fc14b
Adepu Sri Charan <sricharan.adepu36@gmail.com>
2026-01-28 21:50:02 +0530

    feat: externalize database configuration

--- src/main/java/com/arcarshowcaseserver/ArCarShowcaseServerApplication.java
+++ src/main/java/com/arcarshowcaseserver/ArCarShowcaseServerApplication.java
@@ -3,10 +3,15 @@ package com.arcarshowcaseserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ArCarShowcaseServerApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        
         SpringApplication.run(ArCarShowcaseServerApplication.class, args);
     }
 
